9c0e038353c845058925ebbc3c9908e9b3dbb3f8
Nada Issa <rosariojesus909@gmail.com>
2022-03-11 15:37:25 +0100

    experimenting with CORS errors

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 const saucesRoutes = require("./routes/sauces")
 const userRoutes = require("./routes/user");
-const { application } = require('express');
+
+
 mongoose.connect('mongodb+srv://rosario_j:Mycluster123@cluster0.kbvyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -16,9 +17,9 @@ const app = express();
 
 //ligns for the CROS error => to give the app access to the API
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
   next();
 });
 
