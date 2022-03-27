e1925d469fcfad2c6aaaf85d926f7d8d22448837
Nada Issa <rosariojesus909@gmail.com>
2022-03-27 19:14:28 +0200

    modified the Sauce model according to the requirements table

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
 const sauceSchema = mongoose.Schema({
-    //tous ces champs sont à faire selon les infos données par piiquante (ici exp cours)
-    title: { type: String, required: true},
-    description: { type: String, required: true },
-    imageUrl: { type: String, required: true },
     userId: { type: String, required: true },
-    price: { type: Number, required: true },
+    name: { type: String, required: true },
+    manufacturer: {type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true }, //nombre entre 1 et 10
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    usersLiked: { type: [String<userId>], required: true }, //forme à vérifier
+    usersDisliked: { type: [String<userId>], required: true } //forme à vérifier
 
 });
 
